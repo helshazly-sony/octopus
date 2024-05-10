62838c4b21a8f9912a9e64f72a4ea85ad4a3313b
helshazly-sony <Hatem.ElShazly@sony.com>
2024-05-10 09:41:14 +0200

    Fixed path errors and Modified pyconnector

--- server/data-transfer/examples/bin/run_flight_server.sh
+++ server/data-transfer/examples/bin/run_flight_server.sh
@@ -2,4 +2,4 @@
 
 PORT_NUM=${1:-8888}
 
-java --add-opens=java.base/java.nio=ALL-UNNAMED -cp ../../target/data-platform-server-1.0.0-SNAPSHOT-jar-with-dependencies.jar com.sony.SparkFlightServer --port ${PORT_NUM}
+java --add-opens=java.base/java.nio=ALL-UNNAMED -cp ../../../artifacts/data-platform-server-1.0.0-SNAPSHOT-jar-with-dependencies.jar com.sony.SparkFlightServer --port ${PORT_NUM}
