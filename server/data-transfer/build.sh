5668ccd16715d3c2627e13b6137aad807239c4b9
Hatem ElShazly <Hatem.Elshazly@sony.com>
2024-05-24 13:27:43 +0000

    Fixed data transfer server lib path

--- server/data-transfer/build.sh
+++ server/data-transfer/build.sh
@@ -8,6 +8,7 @@ mvn clean install
 # Check if Maven install succeeded
 if [ $? -eq 0 ]; then
     # Copy JAR files and binaries to /opt/octopus/lib
+    mkdir -p /var/octopus/lib/
     cp target/*.jar /var/octopus/lib/
     echo "JAR files copied to /var/octopus/lib successfully."
 else
