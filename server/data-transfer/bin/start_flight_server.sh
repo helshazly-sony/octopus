65bd091f7346ccafb176eaccb61ff920b4f1a018
Hatem ElShazly <Hatem.Elshazly@sony.com>
2024-05-24 11:00:02 +0000

    Fixed hostname and server start and stop scripts

---
+++ server/data-transfer/bin/start_flight_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run stop-script just in case
+stop_flight_server.sh
+
+PORT_NUM=${1:-8888}
+
+## Check if the env variable OCTOPUS_ARROW_FLIGHT_LOGS is set
+## if not, use the default /tmp/
+DEFAULT_LOG_DIR="/tmp/"
+LOG_DIR="${OCTOPUS_ARROW_FLIGHT_LOGS:-$DEFAULT_LOG_DIR}"
+LOG_FILE="${LOG_DIR}/flight_server_$(date +'%Y%m%d_%H%M%S').log"
+
+DEFAULT_OCTOPUS_LIB_DIR="/var/octopus/lib"
+LIB_DIR="${OCTOPUS_LIB_DIR:-$DEFAULT_OCTOPUS_LIB_DIR}"
+
+java --add-opens=java.base/java.nio=ALL-UNNAMED -cp $DEFAULT_OCTOPUS_LIB_DIR/octopus-server-1.0.0-beta.1-SNAPSHOT-jar-with-dependencies.jar com.sony.SparkFlightServer --host "arrow-flight-server" --port ${PORT_NUM} >> ${LOG_FILE} 2>&1 &
--- server/data-transfer/build.sh
+++ server/data-transfer/build.sh
@@ -8,7 +8,7 @@ mvn clean install
 # Check if Maven install succeeded
 if [ $? -eq 0 ]; then
     # Copy JAR files and binaries to /opt/octopus/lib
-    cp target/*.jar /opt/octopus/lib/
+    cp target/*.jar /var/octopus/lib/
     echo "JAR files copied to /var/octopus/lib successfully."
 else
     echo "Maven install failed. JAR files not copied."
